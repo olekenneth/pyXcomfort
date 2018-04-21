a8d17498315cc97a6b7d3f36a3ecc7d1bd2073d8
Ole-Kenneth Bratholt <ok@rait.no>
2018-04-21 12:19:13 +0200

    Add test of devices file

---
+++ test/test_devices.py
@@ -0,0 +1,35 @@
+import unittest
+from xcomfort.devices import *
+
+class XcomfortMock(unittest.TestCase):
+    def setState(self, serial, value):
+        self.assertEqual(value, True)
+    def setBrightness(self, serial, value):
+        self.assertEqual(value > 0, True)
+
+class TestConvert(unittest.TestCase):
+    def test_device(self):
+        d = Device()
+        self.assertEqual(d.state, False)
+        self.assertEqual(str(d), '<xcomfort.devices.Device object serial 0>')
+
+    def test_sensor(self):
+        s = Sensor()
+        self.assertEqual(s.value, 0)
+
+    def test_switch(self):
+        s = Switch()
+        self.assertEqual(s.buttons, 0)
+
+    def test_light(self):
+        xcomfortMock = XcomfortMock()
+        l = Light(xcomfortMock)
+        l.state = True
+        l.brightness = 50
+        self.assertEqual(l.state, True)
+        self.assertEqual(l.brightness, 50)
+
+        l2 = Light()
+        l._isDimable = False
+        with self.assertRaises(NotImplementedError):
+            l.brightness = 50
